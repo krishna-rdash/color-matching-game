cb4e328b33ec4d1e162e65c724550dfc437d03bd
Krishna Bhardwaj <krishna.bhardwaj@rdash.io>
2024-08-23 11:56:03 +0530

    timer Implemented with useInterval

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from "react";
-import { useAppDispatch,useAppSelector } from "../hooks";
+import { useEffect, useRef, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks";
 import { setStatus } from "../store/gameStatus/gameStatus";
 const Timer = () => {
-  const [timeRemaining, setTimeRemaining] = useState(30);
+  const [timeRemaining, setTimeRemaining] = useState(15);
   const gameStatus = useAppSelector((state) => state.gameStatus.status);
   const dispatch = useAppDispatch();
+  let intervalRef = useRef<NodeJS.Timeout | null>();
   useEffect(() => {
-    if(gameStatus!=="running") return;
-    if (timeRemaining === 0) {
-      dispatch(setStatus("Lost"));
-      return;
-    }
-    setTimeout(() => {
+    intervalRef.current = setInterval(() => {
       setTimeRemaining((time) => time - 1);
     }, 1000);
-  }, [timeRemaining]);
+  }, []);
+  if (gameStatus !== "running" || timeRemaining === 0) {
+    if (intervalRef.current) clearInterval(intervalRef.current);
+    if (timeRemaining === 0) dispatch(setStatus("LOST"));
+  }
+
   return <div style={{ fontSize: "50px" }}>{timeRemaining}</div>;
 };
 export default Timer;
