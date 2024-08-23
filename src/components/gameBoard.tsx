e70c073f05e2fdab22a7a7a4c80d93810e5baa9d
Krishna Bhardwaj <krishna.bhardwaj@rdash.io>
2024-08-23 13:20:45 +0530

    game logic implemented with custom attribute instead of classList

--- src/components/gameBoard.tsx
+++ src/components/gameBoard.tsx
@@ -1,6 +1,7 @@
-import { useRef} from "react";
-import { useAppDispatch,useAppSelector } from "../hooks";
+import { useRef, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks";
 import { setStatus } from "../store/gameStatus/gameStatus";
+import { initializeColorMap, isGameOver } from "../utility";
 import "../styles/gameBoard.css";
 const colorIndex = Array.from({ length: 16 }, () =>
   Math.floor(Math.random() * 6)
@@ -12,63 +13,62 @@ const colors = [
   "#F1C40F",
   "#8E44AD",
   "#1ABC9C",
+  "#ccc",
 ];
 const GameBoard = () => {
   const dispatch = useAppDispatch();
   const gameStatus = useAppSelector((state) => state.gameStatus.status);
-  let { current: clickedElement } = useRef<HTMLDivElement | null>(null);
-  const myMap: Map<number, number> = new Map();
-  for (let i: number = 0; i < 6; i++) myMap.set(i, 0);
+  const [firstClickedTile, setFirstClickedTile] =
+    useState<HTMLDivElement | null>(null);
+  const { current: colorMap } = useRef<Map<number, number>>(new Map());
+  initializeColorMap(colorMap);
 
   const handleClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
-    if(gameStatus!=="running") return;
+    if (gameStatus !== "running") return;
     const clickedTile = e.target as HTMLDivElement;
-    if (!clickedElement) {
-      clickedElement = clickedTile;
+    const tileNumber = Number(clickedTile.getAttribute("data-color-index"));
+    const tileColorIdx = colorIndex[tileNumber];
+
+    if (tileColorIdx === 6) return;
+    if (!firstClickedTile) {
+      setFirstClickedTile(clickedTile);
       return;
     }
-    if(clickedElement===clickedTile) {
-      clickedElement = null;
+    const secondClickedTile = clickedTile;
+    if (firstClickedTile === secondClickedTile) {
+      setFirstClickedTile(null);
       return;
     }
-    const firstTileClassList = clickedElement.classList;
-    const secondTileClassList = clickedTile.classList;
-    if (firstTileClassList.length <= 1 && secondTileClassList.length <= 1) {
-      clickedElement = null;
+    const firstTileNumber = Number(
+      firstClickedTile.getAttribute("data-color-index")
+    );
+    const secondTileNumber = tileNumber;
+    const firstTileColorIdx = colorIndex[firstTileNumber];
+    const secondTileColorIdx = tileColorIdx;
+    if (firstTileColorIdx !== secondTileColorIdx) {
+      setFirstClickedTile(null);
       return;
     }
-    if (firstTileClassList[1] !== secondTileClassList[1]) {
-      clickedElement = null;
-      return;
-    }
-    clickedTile.style.backgroundColor = "#ccc";
-    clickedElement.style.backgroundColor = "#ccc";
-    const colorIdx: number = Number(firstTileClassList[1]);
-    firstTileClassList.remove(firstTileClassList[1]);
-    secondTileClassList.remove(firstTileClassList[1]);
-    const prevCount = myMap.get(colorIdx);
+    colorIndex[firstTileNumber] = 6;
+    colorIndex[secondTileNumber] = 6;
+    const prevCount = colorMap.get(tileColorIdx);
     if (!prevCount) return;
-    myMap.set(colorIdx, prevCount - 2);
-    clickedElement = null;
-    let noPairCount: number = 0;
-    for (let i: number = 0; i < 6; i++) {
-      let colorCount = myMap.get(i);
-      if (colorCount === undefined) return;
-      if (colorCount <= 1) noPairCount++;
-    }
-    if (noPairCount === 6) dispatch(setStatus("Won"));
+    colorMap.set(tileColorIdx, prevCount - 2);
+    setFirstClickedTile(null);
+    if (isGameOver(colorMap)) dispatch(setStatus("Won"));
   };
   return (
     <>
       <div className="boardContainer">
         {colorIndex.map((item, idx) => {
-          const prevCount: number = myMap.get(item)!;
-          myMap.set(item, prevCount + 1);
+          const prevCount: number = colorMap.get(item)!;
+          colorMap.set(item, prevCount + 1);
           return (
             <div
-              className={`tile ${item}`}
+              className={`tile`}
               key={idx}
               onClick={handleClick}
+              data-color-index={idx}
               style={{ backgroundColor: colors[item] }}
             ></div>
           );
