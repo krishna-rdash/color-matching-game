c858c27be16ec5e05ebb0b45f1a433b4d520d837
krishna-rdash <krishna.bhardwaj@rdash.io>
2024-08-27 15:25:40 +0530

    using key instead of reload function

--- src/components/gamePage.tsx
+++ src/components/gamePage.tsx
@@ -2,17 +2,21 @@ import GameBoard from "./gameBoard";
 import Timer from "./timer";
 import { useAppSelector, useAppDispatch } from "../hooks/redux_hooks";
 import { setStatus } from "../store/gameStatus/gameStatus";
+import { useState } from "react";
 const GamePage = () => {
   const gameStatus = useAppSelector((state) => state.gameStatus.status);
   const dispatch = useAppDispatch();
+  const [key,setKey] = useState<number>(1);
   const reloadGame = () => {
     dispatch(setStatus("Running"));
-    window.location.reload();
+    setKey(key=>key+1);
   };
+  const userExists = localStorage.getItem("user");
+  if(!userExists) return <div>Login First</div>
   return (
     <>
-      <Timer />
-      <GameBoard />
+      <Timer key={key}/>
+      <GameBoard key={-key}/>
       <div style={{ fontSize: "50px" }}>{gameStatus}</div>
       {gameStatus !== "Running" && (
         <button onClick={reloadGame}>Play Again</button>
